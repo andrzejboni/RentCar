910a06eab9849d38c7c2ee5c3d41e2884c7c4695
andrzejboni <andrzej.boni@outlook.com>
2019-11-09 23:22:38 +0100

    04/13:31

--- src/main/java/com/pact/rentcar/controller/AuthorizationController.java
+++ src/main/java/com/pact/rentcar/controller/AuthorizationController.java
@@ -1,13 +1,22 @@
 package com.pact.rentcar.controller;
 
+import com.pact.rentcar.model.AppUser;
 import com.pact.rentcar.model.dto.request.RegisterUserRequest;
+import com.pact.rentcar.service.AppUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.Optional;
+
 @Controller
 public class AuthorizationController {
+
+    @Autowired
+    private AppUserService appUserService;
+
 
     @GetMapping("/login")
     public String getLoginPage() {
@@ -22,10 +31,18 @@ public class AuthorizationController {
     }
 
     @PostMapping("/register")
-    public String sendRegister(RegisterUserRequest request) {
-    // logika rejsetstracji
+    public String sendRegister(Model model, RegisterUserRequest request) {
+        // logika rejsetstracji
 
+        Optional<AppUser> appUserOptional = appUserService.register(request);
+        if (appUserOptional.isPresent()) {
+            return "redirect:/login";
+        }
 
+        model.addAttribute("message", "Could not register!");
+        model.addAttribute("formObject", request);
+
+        return "authorization/register";
     }
 
 
